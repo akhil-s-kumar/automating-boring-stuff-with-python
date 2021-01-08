63493b9cd7364cc75b2eb86ee9389ee371acf49d
Akhil S Kumar <54428228+akhil-s-kumar@users.noreply.github.com>
2021-01-08 11:05:43 +0530

    The Collatz Sequence

--- chapter-03/projects/The-Collatz-Sequence.py
+++ chapter-03/projects/The-Collatz-Sequence.py
@@ -1,1 +1,17 @@
+def collatz(number):
+    if number%2==0:
+        r = number//2
+        print(r)
+        if r !=1:
+            collatz(r)
+        return r
+    else:
+        r = 3*number+1
+        print(r)
+        if r !=1:
+            collatz(r)
+        return r
+
+n = int(input("Enter Number: "))
+collatz(n)
 
